38ccc598bd24e860f0aa49ecb50ce0f92e48b033
callumhayden <callum.hayden@gmail.com>
2023-11-30 13:11:50 +0000

    more css added to snippet page

--- src/components/Snippet.jsx
+++ src/components/Snippet.jsx
@@ -36,20 +36,20 @@ function Snippet() {
           <div className="image-box">
             <img className="snippet-img" src={dashboard1} alt="Dashboard 1" />
             <div className="image-text-header">Cohort Manager project</div>
-            <div className="image-text">This was my first project that I completed with react by myself and also was apart of my course.<br></br>
+            <div className="image-text1">This was my first project that I completed with react by myself and also was apart of my course.<br></br>
              This is a talk forum where you can create new post's and go on individual post's.</div>
           </div>
           <div className="image-box">
             <img className="snippet-img" src={green2} alt="Dashboard 1" />
             <div className="image-text-header">Green Grocer's project</div>
-            <div className="image-text">This was my first time going from JS Dom to using react whilst completing my course.<br></br>
+            <div className="image-text2">This was my first time going from JS Dom to using react whilst completing my course.<br></br>
              This was just a shopping cart that you can add and take away items from,<br></br>
               with the total of the items being addded up.</div>
           </div>
           <div className="image-box">
             <img className="snippet-img" src={star1} alt="Dashboard 1" />
             <div className="image-text-header">Star wars project</div>
-            <div className="image-text">This project was my first personal project, this was created with HTML, CSS and REACT.JS.<br></br>
+            <div className="image-text3">This project was my first personal project, this was created with HTML, CSS and REACT.JS.<br></br>
              This is a Stwar wars fan base platform, where you can see all the films, characters and planets.<br></br> 
              There's also a favourite section where you can add items to see for future reference.</div>
           </div>
